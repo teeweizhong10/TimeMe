2ab5e11aa8281b0da8e9d44b0f1debb023ddedab
teeweizhong10 <teeweizhong10@gmail.com>
2020-10-22 00:45:49 -0500

    Initial Commit

---
+++ TimeMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeMe
+//
+//  Created by Wei Zhong Tee on 10/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
